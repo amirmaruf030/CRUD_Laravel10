dfbd28960efb3372e05513e01efc239fd30fc9a4
Amir <amirmaruf030@gmail.com>
2023-09-23 07:35:04 +0700

    Membuat file view di posts/cretae dan membuat fungsi insert data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 // Import Model Post
 use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
 // Return type View
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class PostController extends Controller
 {
@@ -17,5 +19,34 @@ class PostController extends Controller
 
         // render view with post
         return view('posts.index', compact('posts'));
+    }
+
+    public function create(): View
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        //validate form
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //create post
+        Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        // redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 }
